6fedb451f6fc372c06340aec993104bc8720de4f
Keston-Antonio-Schmidtgen <keston.antonio.schmidtgen@gmail.com>
2022-06-30 15:45:07 +0200

    upadted my page

--- keston-antonio-schmidtgen-personal-website/src/components/AboutMe.jsx
+++ keston-antonio-schmidtgen-personal-website/src/components/AboutMe.jsx
@@ -7,39 +7,44 @@ export default function AboutMe() {
         <div className="flex flex-col items-center justify-between w-full md:flex-row">
           <div className="">
             <div className="px-3 text-sm font-medium py-1.5 rounded-md bg-gray-800 opacity-100 mb-10 text-gray-400" />
-            <h1 className="text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-gray-200 via-gray-50 to-gray-400">
+            <h1 className=" text-center text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-gray-200 via-gray-50 to-gray-400">
               About Me!
             </h1>
-            <p className="text-lg text-gray-400 my-7">
-              👋🏽 Hi, I'm Keston preferably known as Antonio.
-              <br />
-              <br className="hidden xl:block" />
-              🎓 *Graduate Digital Career Institute Berlin in Web Development.
-              <br />
-              <br className="hidden xl:block" />
-              🌇 *Lives in Berlin, with a passion for Front-end dev.
-              <br />
-              <br className="hidden xl:block" />
-              📚 *Previously worked in customer service.
-              <br />
-              <br className="hidden xl:block" />
-              💻 * I hope to one day work as Front-end developer.
-              <br />
-              <br className="hidden xl:block" />
-              🏔 *In my free time, you can find me hiking or exploring nature.
-              <br />
-              <br className="hidden xl:block" />
-              💪🏽 *My Strengths are teamwork, organized, willing to learn.
-              <br />
-            </p>
+            <div className="text-center">
+              <p className="text-lg text-gray-400 my-7">
+                👋🏽 Hi, I'm Keston preferably known as Antonio.
+                <br />
+                <br className="hidden xl:block" />
+                🎓 *Graduate Digital Career Institute Berlin in Web Development.
+                <br />
+                <br className="hidden xl:block" />
+                🌇 *Lives in Berlin, with a passion for Front-end dev.
+                <br />
+                <br className="hidden xl:block" />
+                📚 *Previously worked in customer service.
+                <br />
+                <br className="hidden xl:block" />
+                💻 * I hope to one day work as Front-end developer.
+                <br />
+                <br className="hidden xl:block" />
+                🏔 *In my free time, you can find me hiking or exploring nature.
+                <br />
+                <br className="hidden xl:block" />
+                💪🏽 *My Strengths are teamwork, organized, willing to learn.
+                <br />
+              </p>
+            </div>
 
             <section class="h-auto bg-white overflow-hidden">
-              <div class="lg:w-1/2 w-full lg:max-w-none max-w-md lg:pt-0 pt-20 mx-auto lg:pl-20">
-                <img
-                  src="https://cdn.devdojo.com/images/march2022/character.png"
-                  class="w-full h-auto"
-                  alt=""
-                />
+              <div class="max-w-6xl pt-24 lg:pt-32 pb-16 lg:pb-24 xl:px-5 px-12 relative z-20 justify-center mx-auto flex lg:flex-row flex-col items-center sm:text-center lg:text-left">
+                <div class="lg:w-1/2 w-full"></div>
+                <div class="lg:w-1/2 w-full lg:max-w-none max-w-md lg:pt-0 pt-20 mx-auto lg:pl-20">
+                  <img
+                    src="https://cdn.devdojo.com/images/march2022/character.png"
+                    class="w-full h-auto"
+                    alt=""
+                  />
+                </div>
               </div>
             </section>
           </div>
--- keston-antonio-schmidtgen-personal-website/src/components/Navigation.jsx
+++ keston-antonio-schmidtgen-personal-website/src/components/Navigation.jsx
@@ -1,26 +1,42 @@
 import React from "react";
 
-
 export default function Navigation() {
- return ( 
-<section className="relative w-full overflow-hidden bg-gray-900">
-  <div className="flex items-center justify-between h-24 px-8 mx-auto max-w-7xl">
-    <a href="#_" className="relative z-10 flex items-center w-auto text-2xl font-extrabold leading-none text-white select-none">Keston Antonio Schmidtgen</a>
-    <nav className="items-center justify-center hidden space-x-8 font-light text-gray-200 md:flex">
-      <a href="#_" className="relative inline-block text-base text-gray-400 transition duration-150 ease hover:text-gray-200">
-        <span className="block">About Me</span>
-      </a>
-      <a href="#_" className="relative inline-block text-base text-gray-400 transition duration-150 ease hover:text-gray-200">
-        <span className="block">Project</span>
-      </a>
-      <a href="#_" className="relative inline-block text-base text-gray-400 transition duration-150 ease hover:text-gray-200">
-        <span className="block">Experience</span>
-      </a>
-      <a href="#_" className="relative inline-block text-base text-gray-400 transition duration-150 ease hover:text-gray-200">
-        <span className="block">Contact</span>
-      </a>
-    </nav>
-  </div>
-</section>
-    )
-};
+  return (
+    <section className="relative w-full overflow-hidden bg-gray-900">
+      <div className="flex items-center justify-between h-24 px-8 mx-auto max-w-7xl">
+        <a
+          href="#_"
+          className="relative z-10 flex items-center w-auto text-2xl font-extrabold leading-none text-white select-none"
+        >
+          Keston Antonio Schmidtgen
+        </a>
+        <nav className="items-center justify-center hidden space-x-8 font-light text-gray-200 md:flex">
+          <a
+            href="#_"
+            className="relative inline-block text-base text-gray-400 transition duration-150 ease hover:text-gray-200"
+          >
+            <span className="block">About Me</span>
+          </a>
+          <a
+            href="#_"
+            className="relative inline-block text-base text-gray-400 transition duration-150 ease hover:text-gray-200"
+          >
+            <span className="block">Experience</span>
+          </a>
+          <a
+            href="#_"
+            className="relative inline-block text-base text-gray-400 transition duration-150 ease hover:text-gray-200"
+          >
+            <span className="block">Project</span>
+          </a>
+          <a
+            href="#_"
+            className="relative inline-block text-base text-gray-400 transition duration-150 ease hover:text-gray-200"
+          >
+            <span className="block">Contact</span>
+          </a>
+        </nav>
+      </div>
+    </section>
+  );
+}
